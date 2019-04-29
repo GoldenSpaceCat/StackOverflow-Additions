8a54b67a389e984d5315a4733d071a0482205067
Jan-Stefan Janetzky <github@GottZ.de>
2019-04-29 21:18:45 +0200

    initial

---
+++ SO-A.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         StackOverflow Additions
+// @namespace    https://GottZ.de
+// @version      0.1
+// @description  change some stackoverflow styling
+// @author       GottZ
+// @include      /^https?:\/\/stackoverflow\.com\//
+// @updateURL    https://github.com/GoldenSpaceCat/StackOverflow-Additions/raw/master/SO-A.user.js
+// @downloadURL  https://github.com/GoldenSpaceCat/StackOverflow-Additions/raw/master/SO-A.user.js
+// @icon         https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico
+// @grant        unsafeWindow
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const style = document.createElement("style");
+    unsafeWindow.document.head.appendChild(style);
+    style.textContent = `
+        .s-btn-group, pre, .post-signature.owner {
+            box-shadow: 0 2px 8px rgba(59, 64, 69, 0.1);
+        }
+        pre {
+            border: 1px solid #d8dcdf;
+        }
+        .post-signature.owner {
+            border: 1px solid hsla(205, 46%, 80%, 1);
+        }
+        .tagged-interesting {
+            background: linear-gradient(to right, #fffbec, transparent);
+        }
+    `;
+})();
