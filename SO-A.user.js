257fc4631db3fa2ebd405132106b97827627e932
Jan-Stefan Janetzky <github@GottZ.de>
2019-05-09 20:38:04 +0200

    added superuser and question status shadow

--- SO-A.user.js
+++ SO-A.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         StackOverflow Additions
 // @namespace    https://GottZ.de
-// @version      0.1.2
+// @version      0.1.3
 // @description  change some stackoverflow styling
 // @author       GottZ
-// @include      /^https?:\/\/stackoverflow\.com\//
+// @include      /^https?:\/\/(stackoverflow|superuser)\.com\//
 // @updateURL    https://github.com/GoldenSpaceCat/StackOverflow-Additions/raw/master/SO-A.user.js
 // @downloadURL  https://github.com/GoldenSpaceCat/StackOverflow-Additions/raw/master/SO-A.user.js
 // @icon         https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico
@@ -18,7 +18,7 @@
     const style = document.createElement("style");
     unsafeWindow.document.head.appendChild(style);
     style.textContent = `
-        .s-btn-group, pre, .post-signature.owner, .post-editor > .ps-relative {
+        .s-btn-group, pre, .post-signature.owner, .post-editor > .ps-relative, .question-status {
             box-shadow: 0 2px 8px rgba(59, 64, 69, 0.1);
         }
         pre {
